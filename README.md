d863de5b0cf7c62751b5ef571645e55994b1f393
Tim Gabets <t.gabets@tinkoff.ru>
2020-05-28 20:32:27 +0300

    tokio-based async implementation

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # echoserver
 
-Dummy implementation of single-threaded TCP echo server.
+Dummy implementation of asynchronous TCP echo server.
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,36 @@
-use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
+use futures::stream::StreamExt;
+use tokio::net::TcpListener;
 
-fn main() {
-	let host = "127.0.0.1:8080";
+#[tokio::main]
+async fn main() {
+    let addr = "127.0.0.1:6142";
+    let default_timeout = 2; // seconds
+    let mut listener = TcpListener::bind(addr).await.unwrap();
 
-	let listener = TcpListener::bind(host).expect("bind() error");
-	println!("Listening on {}", host);
-
-	for stream in listener.incoming() {
-		match stream {
-			Ok(s) => {
-				println!("New client from {}", s.peer_addr().unwrap());
-				handle_connection(s);
-			},
-			Err(e) => {
-				println!("{}", e);
-				continue;
-			},
-		}
-		println!("Client disconnected");
-	}
-}
-
-fn handle_connection(mut s : TcpStream) {
-	let mut buffer = [0; 2048];
-	s.read(&mut buffer).expect("read() error");
-	s.write(&buffer).expect("write() error");
+    let server = {
+        async move {
+            let mut incoming = listener.incoming();
+            while let Some(conn) = incoming.next().await {
+                match conn {
+                    Err(e) => eprintln!("accept failed = {:?}", e),
+                    Ok(mut sock) => {
+                        tokio::spawn(async move {
+                            let (mut reader, mut writer) = sock.split();
+                            tokio::time::delay_for(tokio::time::Duration::from_secs(default_timeout)).await;
+                            match tokio::io::copy(&mut reader, &mut writer).await {
+                                Ok(amt) => {
+                                    println!("wrote {} bytes", amt);
+                                }
+                                Err(err) => {
+                                    eprintln!("IO error {:?}", err);
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        }
+    };
+    println!("Server running on {} (default timeout={} seconds)", addr, default_timeout);
+    server.await;
 }
